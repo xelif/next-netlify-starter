285b5bb1755355e4bcd0762bab3f53832cb533a5
Felix <47697694+xelif@users.noreply.github.com>
2022-04-10 15:25:07 +0200

    1

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Unik Trapp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Her kommer Unik Trapp" />
       </main>
-
-      <Footer />
     </div>
   )
 }
